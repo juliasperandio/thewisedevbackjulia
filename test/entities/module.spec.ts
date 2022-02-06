53b39d3aaed4f16f035358db3f2384cfa0404029
Julia Sperandio Pereira1 <juliasperandio@ibm.com>
2022-02-06 18:59:12 -0300

    refactor: remove redundant test

--- test/entities/module.spec.ts
+++ test/entities/module.spec.ts
@@ -1,4 +1,5 @@
 import { Lecture, Module } from '../../src/entities'
+import { ExistingElementError } from '../../src/entities/errors/existing-element-error'
 
 describe('Module', () => {
   it('should be able to add lectures to modules', () => {
@@ -6,16 +7,6 @@ describe('Module', () => {
     const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
     module.add(lecture)
     expect(module.includes(lecture)).toBeTruthy()
-  })
-
-  it('should not be able to add the same lecture twice in a module', () => {
-    const module = new Module('Fundamentals')
-    const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    const sameLecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
-    module.add(lecture)
-    module.add(sameLecture)
-    expect(module.includes(lecture)).toBeTruthy()
-    expect(module.numberOfLectures).toBe(1)
   })
 
   it('should not be able to have two lectures with same name in a module', () => {
