9babc3218733ced1bb3c7079c178b9d8d2bfed46
Julia Sperandio Pereira1 <juliasperandio@ibm.com>
2022-02-06 19:08:37 -0300

    feat: existing material

--- src/entities/lecture.ts
+++ src/entities/lecture.ts
@@ -1,4 +1,6 @@
+import { Either } from '../shared/either'
 import { Container } from './container'
+import { ExistingElementError } from './errors/existing-element-error'
 import { Material } from './material'
 import { Element } from './part'
 
@@ -12,8 +14,8 @@ export class Lecture implements Element {
     this.videoUrl = videoUrl
   }
 
-  add (material: Material): void {
-    this.materials.add(material)
+  add (material: Material): Either<ExistingElementError, void> {
+    return this.materials.add(material)
   }
 
   includes (material: Material): boolean {
